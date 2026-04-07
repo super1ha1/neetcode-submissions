29a0f55d9e4eaef7c21d1f0ec470d17e91b83640
Billy <khue0011@gmail.com>
2026-04-07 10:58:53 +0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+
+        visit = set()
+        direction = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+
+        def bt(i, j, cur):
+            if len(cur) == 0:
+                # success
+                return True
+
+            
+            for dx, dy in direction:
+                x = i + dx
+                y = j + dy
+                if 0 <= x < m and 0 <= y < n and board[x][y] == cur[0] and (x, y) not in visit:
+                    visit.add((x, y))
+                    if bt(x, y, cur[1:]):
+                        return True
+                    visit.remove((x, y))
+            return False
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    visit.add((i, j))
+                    if bt(i, j, word[1:]):
+                        return True
+                    visit.remove((i, j))
+        
+        return False
+        
+
